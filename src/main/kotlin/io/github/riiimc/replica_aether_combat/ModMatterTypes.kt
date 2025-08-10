701f873e779893ea1dca99bb04448539e29c1272
Riii3393 <aleph.0.leaf@gmail.com>
2025-08-10 11:01:03 +0900

    *

--- src/main/kotlin/io/github/riiimc/replica_aether_combat/ModMatterTypes.kt
+++ src/main/kotlin/io/github/riiimc/replica_aether_combat/ModMatterTypes.kt
@@ -2,9 +2,7 @@ package io.github.riiimc.replica_aether_combat
 
 import com.buuz135.replication.ReplicationRegistry
 import com.buuz135.replication.api.IMatterType
-import net.neoforged.neoforge.registries.DeferredHolder
 import net.neoforged.neoforge.registries.DeferredRegister
-import net.minecraft.resources.ResourceLocation
 import net.neoforged.bus.api.IEventBus
 
 
@@ -14,9 +12,6 @@ object ModMatterTypes {
         DeferredRegister.create(ReplicationRegistry.MATTER_TYPES_KEY, Main.ID)
 
     private val ETHERIO_INSTANCE = EtherioMatterType()
-
-    val ETHERIO: DeferredHolder<IMatterType, IMatterType> =
-        MATTER_TYPES.register("etherio") { _: ResourceLocation -> ETHERIO_INSTANCE }
 
     fun register(bus: IEventBus) {
         MATTER_TYPES.register(bus)
